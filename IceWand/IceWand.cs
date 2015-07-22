967240761f99f967fbd9220ac9240ded07d6a7c8
Zaicon Kiroshu <kzaicon@yahoo.com>
2015-07-22 10:56:29 -0500

    TShock 4.3.6 Pre-Update

--- IceWand/IceWand.cs
+++ IceWand/IceWand.cs
@@ -10,7 +10,7 @@ using TShockAPI;
 
 namespace IceWand
 {
-	[ApiVersion(1, 16)]
+	[ApiVersion(1, 19)]
 	public class IceWand : TerrariaPlugin
 	{
 		List<IceWandAction> Actions = new List<IceWandAction>();
